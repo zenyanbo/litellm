c55b4e3e70693271009be697036c729305427e2b
zenyanbo <zengyanbo2huge@126.com>
2025-12-28 20:53:24 +0800

    feat: Implement retry on empty response

--- litellm/main.py
+++ litellm/main.py
@@ -251,6 +251,8 @@ from litellm.utils import (
     TranscriptionResponse,
 )
 
+from .llms.custom_failure_policy import CustomFailurePolicy
+
 ####### ENVIRONMENT VARIABLES ###################
 openai_chat_completions = OpenAIChatCompletion()
 openai_text_completions = OpenAITextCompletion()
@@ -412,6 +414,7 @@ async def acompletion(
     web_search_options: Optional[OpenAIWebSearchOptions] = None,
     # Session management
     shared_session: Optional["ClientSession"] = None,
+    retry_on_empty_response: bool = True,
     **kwargs,
 ) -> Union[ModelResponse, CustomStreamWrapper]:
     """
@@ -620,6 +623,14 @@ async def acompletion(
             response.set_logging_event_loop(
                 loop=loop
             )  # sets the logging event loop if the user does sync streaming (e.g. on proxy for sagemaker calls)
+            
+        if (
+            retry_on_empty_response
+            and kwargs.get("stream", False) is False
+            and isinstance(response, litellm.ModelResponse)
+        ):
+            CustomFailurePolicy.check_response(response, model=model, llm_provider=custom_llm_provider)
+            
         return response
     except Exception as e:
         custom_llm_provider = custom_llm_provider or "openai"
@@ -1036,6 +1047,7 @@ def completion(  # type: ignore # noqa: PLR0915
     thinking: Optional[AnthropicThinkingParam] = None,
     # Session management
     shared_session: Optional["ClientSession"] = None,
+    retry_on_empty_response: bool = True,
     **kwargs,
 ) -> Union[ModelResponse, CustomStreamWrapper]:
     """
@@ -4040,6 +4052,14 @@ def completion(  # type: ignore # noqa: PLR0915
             raise LiteLLMUnknownProvider(
                 model=model, custom_llm_provider=custom_llm_provider
             )
+            
+        if (
+            retry_on_empty_response
+            and kwargs.get("stream", False) is False
+            and isinstance(response, litellm.ModelResponse)
+        ):
+            CustomFailurePolicy.check_response(response, model=model, llm_provider=custom_llm_provider)
+            
         return response
     except Exception as e:
         ## Map to OpenAI Exception
